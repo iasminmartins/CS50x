0faea303042724bf39ae93b7e280de01a7071004
iasminmartins <iasminmartins@users.noreply.github.com>
2024-12-04 17:00:24 -0300

    automated commit by check50 [check50=True]

---
+++ dictionary.c
@@ -0,0 +1,135 @@
+// Implements a dictionary's functionality
+
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+
+#include "dictionary.h"
+
+// Represents a node in a hash table
+typedef struct node
+{
+    char word[LENGTH + 1];
+    struct node *next;
+} node;
+
+// TODO: Choose number of buckets in hash table
+const unsigned int N = 26;
+
+// Hash table
+node *table[N];
+
+// Returns true if word is in dictionary, else false
+bool check(const char *word)
+{
+    // Hash word to find bucket
+    node *ptr = table[hash(word)];
+
+    // Traverse linked list at hashed index
+    while (ptr != NULL)
+    {
+        // Compare case-insensitively
+        if (strcasecmp(ptr->word, word) == 0)
+        {
+            return true; // Word found
+        }
+        ptr = ptr->next;
+    }
+    return false; // Word not found
+}
+
+// Hashes word to a number
+unsigned int hash(const char *word)
+{
+
+    unsigned long hash_value = 0;
+
+    for (int i = 0; word[i] != '\0'; i++)
+    {
+        // Ensure case insensitivity (tolower) and reduce collisions (prime number)
+        hash_value = (hash_value * 31 + tolower(word[i])) % N;
+    }
+
+    return hash_value % N; // Ensure bounds
+}
+
+// Loads dictionary into memory, returning true if successful, else false
+bool load(const char *dictionary)
+{
+    // Open the dictionary file
+    FILE *source = fopen(dictionary, "r");
+    if (source == NULL)
+    {
+        printf("Could not open file.\n");
+        return false;
+    }
+
+    // Initialize table
+    for (int i = 0; i < N; i++)
+    {
+        table[i] = NULL;
+    }
+
+    // Read each word in file
+    char word[LENGTH + 1];
+    while (fscanf(source, "%s", word) != EOF)
+    {
+        // Create new node
+        node *new_node = malloc(sizeof(node));
+        if (new_node == NULL)
+        {
+            printf("Could not allocate memory.\n");
+            fclose(source);
+            return false;
+        }
+
+        // Add word to hash table
+        strcpy(new_node->word, word);
+        new_node->next = table[hash(word)];
+        table[hash(word)] = new_node;
+    }
+
+    // Close the dictionary file
+    fclose(source);
+    return true;
+}
+
+// Returns number of words in dictionary if loaded, else 0 if not yet loaded
+unsigned int size(void)
+{
+    unsigned int count = 0;
+
+    // Traverse bucket
+    for (int i = 0; i < N; i++)
+    {
+        node *ptr = table[i];
+        while (ptr != NULL)
+        {
+            count++;
+            ptr = ptr->next;
+        }
+    }
+
+    return count;
+}
+
+// Unloads dictionary from memory, returning true if successful, else false
+bool unload(void)
+{
+    // Free each bucket
+    for (int i = 0; i < N; i++)
+    {
+        node *ptr = table[i];
+        while (ptr != NULL)
+        {
+            node *temp = ptr;
+            ptr = ptr->next;
+            free(temp);
+        }
+    }
+
+    return true;
+}
