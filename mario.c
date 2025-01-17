d1c3565f3d3feabfae1474e6f9365f808f2ffb6d
Iasmin Martins <42560173+iasminmartins@users.noreply.github.com>
2025-01-16 21:06:52 -0300

    docs: update comments for clarity and consistency

--- mario.c
+++ mario.c
@@ -7,18 +7,22 @@ void print_row(int length);
 int main(void)
 {
     int height;
+    // Prompt user for a positive integer
     do
     {
         height = get_int("Height: ");
     }
     while (height < 1);
+
+    // Print each row of the pyramid
     for (int j = 0; j < height; j++)
     {
-        print_spaces(height - j - 1);
-        print_row(j + 1);
+        print_spaces(height - j - 1); // Print spaces
+        print_row(j + 1);             // Print hashes
     }
 }
 
+// Print given number of spaces
 void print_spaces(int spaces)
 {
     for (int k = spaces; k > 0; k--)
@@ -27,7 +31,8 @@ void print_spaces(int spaces)
     }
 }
 
-void print_row(int length) // sempre definir tipo de variavel na função
+// Print row of hashes of given lenght
+void print_row(int length) 
 {
     for (int i = 0; i < length; i++)
     {
