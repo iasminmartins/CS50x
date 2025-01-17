e1957e866c085a4e30102c932b6c8d1da39f222c
Iasmin Martins <42560173+iasminmartins@users.noreply.github.com>
2025-01-16 21:31:08 -0300

    docs: update comments for clarity and consistency

--- credit.c
+++ credit.c
@@ -6,72 +6,79 @@ void kind_of_card(long number);
 
 int main(void)
 {
-    long card_number = get_long("Number: ");
-    int sum = luhns(card_number);
-    sum %= 10;
-    if (sum == 0)
+    long card_number = get_long("Number: ");    // Prompt for card number
+    int sum = luhns(card_number);    // Get sum for Luhn's algorithm
+    sum %= 10;    // Get last digit    
+    if (sum == 0)    // If valid
     {
-        kind_of_card(card_number);
+        kind_of_card(card_number);    // Identify card type
     }
-    else
+    else // If invalid
     {
         printf("INVALID\n");
     }
 }
 
+// Luhn's algorithm
 int luhns(long number)
 {
     int counter = 0, sum = 0, sum2 = 0;
     while (number > 0)
     {
-        // Para separar o ultimo digito
-        int last_digit = number % 10;
-        // Para excluir o ultimo digito
-        number /= 10;
-        if (counter == 1)
+        int last_digit = number % 10;    // Get last digit
+        number /= 10;    // Remove last digit
+        if (counter == 1)    // For every second digit (from second to last)
         {
             int doubling;
             doubling = last_digit * 2;
             if (doubling > 9)
             {
-                sum = sum + 1 + (doubling % 10);
+                sum = sum + 1 + (doubling % 10);    // Add sum of digits of doubled value
             }
             else
             {
-                sum = sum + doubling;
+                sum = sum + doubling;    // Add double value
             }
             counter = 0;
         }
-        else
+        else    // For other digits
         {
-            sum2 = sum2 + last_digit;
+            sum2 = sum2 + last_digit;    // Add digit to sum2
             counter++;
         }
     }
-    sum += sum2;
+    sum += sum2;    // Final sum
     return sum;
 }
 
+// Determine type of card based on first digits and length
 void kind_of_card(long number)
 {
     int counter = 0;
-    long num;
-    num = number;
+    long num = number;
+
+    // Count number of digits
     while (num > 0)
     {
         int last_digit = num % 10;
         num /= 10;
         counter++;
     }
+    
     int first_digit;
     num = number;
+
+    // Remove first digit
     while (num >= 10)
     {
         num /= 10;
     }
     first_digit = num;
+    
     num = number;
     int first_two_digits;
+
+    // Remove first two digits
     while (num >= 100)
     {
         num /= 10;
@@ -79,6 +86,7 @@ void kind_of_card(long number)
 
     first_two_digits = num;
 
+    // Check card type
     if ((first_digit == 4 && counter == 13) || (first_digit == 4 && counter == 16))
     {
         printf("VISA\n");
