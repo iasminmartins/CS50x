848a0f4dc8d49a7f29e82a2042a140d138cef351
Iasmin Martins <42560173+iasminmartins@users.noreply.github.com>
2025-01-16 22:14:44 -0300

    docs: update comments for clarity and consistency

--- volume.c
+++ volume.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    // Open files and determine scaling factor
+    // Open input file for reading
     FILE *input = fopen(argv[1], "r");
     if (input == NULL)
     {
@@ -24,6 +24,7 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+    // Open input file for writing
     FILE *output = fopen(argv[2], "w");
     if (output == NULL)
     {
@@ -31,28 +32,25 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+    // Get scaling factor from command-line argument
     float factor = atof(argv[3]);
 
     // Copy header from input file to output file
-
     uint8_t header[HEADER_SIZE];
     fread(header, HEADER_SIZE, 1, input);
     fwrite(header, HEADER_SIZE, 1, output);
 
     // Read samples from input file and write updated data to output file
-
     int16_t buffer;
-
-    // This version of C treats non-zero values as true and zero values as false
-    // So I don't need to make while(fread... != 0), if its 0 it'll be false, so it'll exit the loop
-
+    
+    // Read and adjust samples until the end of the file
     while (fread(&buffer, sizeof(int16_t), 1, input))
     {
-        buffer *= factor;
-        fwrite(&buffer, sizeof(int16_t), 1, output);
+        buffer *= factor;    // Scale sample value by factor
+        fwrite(&buffer, sizeof(int16_t), 1, output);    // Write updated sample to output file
     }
 
-    // Close files
+    // Close input and output files
     fclose(input);
     fclose(output);
 }
